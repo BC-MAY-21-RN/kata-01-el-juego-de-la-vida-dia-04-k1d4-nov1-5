a04de7a615b79a766934505d4a1f8593d2198d36
danthek <abraham8507@gmail.com>
2021-11-23 18:05:05 -0600

    ODE CLIMATE INTEGRATION TEST COVERAGE OF MAINTAINABILITY USING CODE CLIMATE TOOL Co-authored-by: danthek <abraham8507@gmail.com> Co-authored-by: Hugoneytor <halvarez0@ucol.mx> Co-authored-by: Ernesto2899 <eramirez28@ucol.mx>

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-/*
+/*GITHUB USERS
   Co-authored-by: danthek <abraham8507@gmail.com>
   Co-authored-by: Hugoneytor <halvarez0@ucol.mx>
   Co-authored-by: Ernesto2899 <eramirez28@ucol.mx>
